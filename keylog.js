6cb768fbfa9606e45c91c3d8a0fa747b77c48ae2
Dale leyland <daleleyland@icloud.com>
2026-06-03 19:05:19 +1200

    added key logers for WASD

--- keylog.js
+++ keylog.js
@@ -1,5 +1,14 @@
 document.addEventListener('keydown', function(event) {
-    if (event.key === 'a' || event.key ==='A'){
-        console.log('poo');
+    if (event.key === 'W' || event.key ==='w'){
+        console.log('W');
+    }
+    if (event.key === 'A' || event.key ==='a'){
+        console.log('A');
+    }
+    if (event.key === 'S' || event.key ==='s'){
+        console.log('S');
+    }
+    if (event.key === 'D' || event.key ==='d'){
+        console.log('D');
     }
 });
